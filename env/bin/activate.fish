87dc26c0f837c4a3363947c4b3262de01f181d7c
abhishekmadan7 <95368860+abhishekmadan7@users.noreply.github.com>
2024-10-02 13:03:38 -0400

    test

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/abhishekmadan/Desktop/UofT-ECE444-PRA-TDD-Deployment/flask-tddr/env"
+set -gx VIRTUAL_ENV "/Users/abhishekmadan/Desktop/UofT-ECE444-PRA-TDD-Deployment/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
